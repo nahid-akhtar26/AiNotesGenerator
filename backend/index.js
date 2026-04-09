d7b6e0d85a13c55eff54c204727c3b7986ab5c45
Nahid Akhtar <nahidakhtar9968@gmail.com>
2026-04-09 17:06:27 +0530

    Update CORS  Origin for Production Environment

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://ainotesgeneratorfrontend-evxd.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   }),
