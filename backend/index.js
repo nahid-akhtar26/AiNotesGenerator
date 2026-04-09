e21ebb00ba5519332fa48a02a00e7750a9d58725
Nahid Akhtar <nahidakhtar9968@gmail.com>
2026-04-09 17:32:28 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://ainotesgeneratorfrontend-evxd.onrender.com",
+    origin: "https://ainotesgeneratorfrontend.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   }),
